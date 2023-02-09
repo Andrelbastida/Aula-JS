4f604b6c8e52de6be3ee28409a6ebf7068b0e866
andrelbastida <andrelbastida@yahoo.com.br>
2023-02-08 22:43:40 -0300

    first commit

---
+++ JS/aula.js
@@ -0,0 +1,4 @@
+var numero1 = Number(window.prompt ("Digite um número"))
+var numero2 = Number(window.prompt ("Digite mais um número"))
+var result = numero1 + numero2
+window.alert("O resultado da soma é " + result)
